4ea61ea3beb3bc9f0eb23ef692dc51142789edcd
AnangM <social.anangmb@gmail.com>
2018-11-01 10:31:25 +0700

    add some comment

--- 05/Tugas.java
+++ 05/Tugas.java
@@ -1,50 +1,71 @@
+//import class needed
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Tugas{
     public static void main(String[] args) {
+        //deklarasi buffered reader
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //deklarasi variable untuk perulangan dan inpyt
         String ulang = "y", input;
+        //deklarasi variabel nilai
         Double nilai = 0.0;
+        //perulangan untuk hitung ulang
         while(ulang.equals("y") || ulang.equals("Y")){
             System.out.println("=======================================");
             try{
                 System.out.print("Masukkan Nilai : ");
+                //get user input
                 input = bf.readLine();
+                //convert String to Double
                 nilai = Double.parseDouble(input);
             }catch(IOException e){
+                //output error
                 System.out.println("[ERROR] " + e);
             }
             System.out.println("---------------------------------------");
+            //if pertama
             if(nilai > 100){
                 System.out.println("Apakah nilai tersebut benar? Jika YA maka silahkan hubungi Dosen anda, karena nilai anda tidak masuk akal!");
+            //if kedua dengan logika and (&&)
             }else if(nilai > 85.0 && nilai <= 100.0){
                 System.out.println("Nilai = A");
+            //lanjut if
             }else if(nilai > 80.0 && nilai <= 85.0){
                 System.out.println("Nilai = A-");
+            //lanjut if
             }else if(nilai > 76.0 && nilai <= 80.0){
                 System.out.println("Nilai = B+");
+            //lanjut if
             }else if(nilai > 74.0 && nilai <= 76.0){
                 System.out.println("Nilai = B");
-            }else if(nilai > 70.0 && nilai <= 74.0){
-                System.out.println("Nilai = B-");
-            }else if(nilai > 65.0 && nilai <= 70.0){
-                System.out.println("Nilai = C+");
-            }else if(nilai > 60.0 && nilai <= 65.0){
-                System.out.println("Nilai = C");
-            }else if(nilai > 55.0 && nilai <= 60.0){
-                System.out.println("Nilai = C-");
-            }else if(nilai > 50.0 && nilai <= 55.0){
-                System.out.println("Nilai = D");
-            }else if(nilai <= 50.0){
-                System.out.println("Nilai = E");
+            //lanjut if
+        }else if(nilai > 70.0 && nilai <= 74.0){
+            System.out.println("Nilai = B-");
+            //lanjut if
+        }else if(nilai > 65.0 && nilai <= 70.0){
+            System.out.println("Nilai = C+");
+            //lanjut if
+        }else if(nilai > 60.0 && nilai <= 65.0){
+            System.out.println("Nilai = C");
+            //lanjut if
+        }else if(nilai > 55.0 && nilai <= 60.0){
+            System.out.println("Nilai = C-");
+            //lanjut if
+        }else if(nilai > 50.0 && nilai <= 55.0){
+            System.out.println("Nilai = D");
+            //lanjut if
+        }else if(nilai <= 50.0){
+            System.out.println("Nilai = E");
+            //terakhir apabila tidak semua nilai false
             }else{
                 System.out.println("[ERROR]" + nilai);
             }
             System.out.println("=======================================");
             try{
             System.out.print("Hitung Ulang?(Y/N)");
+            //input untuk perulangan
             ulang = bf.readLine();
         }catch (IOException e){
             System.out.println("[Error] + e");
